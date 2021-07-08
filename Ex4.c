6e7ede2fc175581a8d5fa09bbb54199c98607fab
s1280031 <87136317+s1280031@users.noreply.github.com>
2021-07-08 23:34:55 +0900

    Update and rename README.md to Ex4.c

---
+++ Ex4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+
+int Dice(int);
+int main()
+{
+  srand((unsigned int)time(NULL));
+  int n=0;
+  int i;
+  char name[20];
+
+    
+  printf("What is your name?\n");
+  printf(">");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+    
+
+ 
+  printf("Rolling the dice...\n");
+
+  for(i=0;i<2;i++)
+    n=n+Dice(i+1);
+
+  
+  printf("Total value:%d\n",n);
+  
+  if(n>=7) printf("%s won!\n",name);
+  else printf("%s lost!\n",name);
+
+  printf("END\n");
+  
+  return 0;
+}
+
+int Dice(int i)
+{
+  int m;
+  m=rand()%6+1;
+  printf("Die %d:%d\n",i,m);
+  return m;
+}
